56f736ef0bd15ad656ce197587393feccfd2eeba
KIM Taegyoon <steloflute@gmail.com>
2015-09-07 14:21:22 +0900

    fix

--- src/paren/paren.java
+++ src/paren/paren.java
@@ -31,7 +31,7 @@ import java.lang.Math;
 
 public class paren {
 	public static final String VERSION = "1.12";
-    public paren() {
+    public paren() throws Exception {
         init();
     }
     
